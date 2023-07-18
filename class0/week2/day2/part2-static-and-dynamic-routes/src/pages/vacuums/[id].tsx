e056110ecb44c6a7bbf9400f9a49f88be2246ee8
Aidenbuis <aidenbuis@gmail.com>
2023-07-18 22:13:15 +0200

    Update code

--- class0/week2/day2/part2-static-and-dynamic-routes/src/pages/vacuums/[id].tsx
+++ class0/week2/day2/part2-static-and-dynamic-routes/src/pages/vacuums/[id].tsx
@@ -3,44 +3,53 @@ import vacuums from "@/data/vacuums.json";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const VacuumOne = () => {
+const VacuumPage = () => {
+  // We create the router
   const router = useRouter();
-  const { id } = router.query;
 
-  // Check the type of a value
-  const idIsOfTypeString = typeof id === "string";
+  // We immediately try to convert the id from the URL to a number with the Number() function
+  const idFromUrl = Number(router.query.id);
+  // Now we can access the .id property, because our file is called [id].tsx
+  // Would we have called it [vacuumNumber].tsx,
+  // we would have to use router.query.vacuumNumber
 
-  if (!id || !idIsOfTypeString) {
-    return <h1>No ID in URL! 🐸</h1>;
+  // We then check if this is a number,
+  // with the isNan (is not a number) function
+  // If it is NOT a number, we return this <h1>
+  if (isNaN(idFromUrl)) {
+    return <h1>No valid ID in URL! 🐸</h1>;
   }
 
-  const idFromUrl = parseInt(id);
+  // If we made it this far idFromUrl is a number, hooray! 🎉
+
+  // Now on to find the vacuumFromUrl
   const vacuumFromUrl = vacuums.find((vacuum) => vacuum.id === idFromUrl);
 
-  if (vacuumFromUrl === undefined) {
+  // If we couldn't find one, we return this <h1>
+  if (!vacuumFromUrl) {
     return <h1>Product not found! 🐸</h1>;
-  } else {
-    return (
-      <div className="product-page">
-        <div>
-          <Link className="back-button" href="/">
-            👈 Back
-          </Link>
-          <VacuumCard
-            id={vacuumFromUrl.id}
-            name={vacuumFromUrl.name}
-            reviews={vacuumFromUrl.reviews}
-            type={vacuumFromUrl.type}
-            suction_power={vacuumFromUrl.suction_power}
-            floor_type={vacuumFromUrl.floor_type}
-            retail_price={vacuumFromUrl.retail_price}
-            availability={vacuumFromUrl.availability}
-            pickup={vacuumFromUrl.pickup}
-          />
-        </div>
-      </div>
-    );
   }
+
+  return (
+    <div className="product-page">
+      <div>
+        <Link className="back-button" href="/">
+          👈 Back
+        </Link>
+        <VacuumCard
+          id={vacuumFromUrl.id}
+          name={vacuumFromUrl.name}
+          reviews={vacuumFromUrl.reviews}
+          type={vacuumFromUrl.type}
+          suction_power={vacuumFromUrl.suction_power}
+          floor_type={vacuumFromUrl.floor_type}
+          retail_price={vacuumFromUrl.retail_price}
+          availability={vacuumFromUrl.availability}
+          pickup={vacuumFromUrl.pickup}
+        />
+      </div>
+    </div>
+  );
 };
 
-export default VacuumOne;
+export default VacuumPage;
