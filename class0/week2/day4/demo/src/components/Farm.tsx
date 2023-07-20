ba6d41a08feda60794e11eb3fa6bc8531295d0b0
Swen Mulderij <swenmulderij@gmail.com>
2023-07-20 12:06:00 +0200

    after week4day2part1

---
+++ class0/week2/day4/demo/src/components/Farm.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+// {
+//     "id": 1,
+//     "name": "Bessie",
+//     "kind": "cow",
+//     "age": 4,
+//     "hasBeenFed": false,
+//     "imgUrl": "https://i.giphy.com/media/h55EUEsTG9224/giphy.webp"
+//   },
+
+interface Animal {
+  id: number;
+  name: string;
+  kind: "cow" | "chicken" | "sheep" | "pig";
+  age: number;
+  hasBeenFed: boolean;
+  imgUrl: string;
+}
+
+const Farm = () => {
+  //   let animals = [];
+  const [getAnimals, setAnimals] = useState<Animal[] | null>(null);
+
+  useEffect(() => {
+    // Define async function for axios
+    const getAnimals = async () => {
+      const response = await axios.get(
+        "https://reader.mindmingle.nl/api/exercises/react/animals"
+      );
+      //   console.log(response.data);
+      // animals = response.data
+      setAnimals(response.data);
+    };
+    // Call the function
+    getAnimals();
+    console.log("I was born!");
+  }, []);
+
+  //   if (getAnimals === null) {
+  //     return (
+  //       <div>
+  //         <h1>Hello from farm</h1>
+  //         <p>Loading...</p>
+  //       </div>
+  //     );
+  //   }
+
+  return (
+    <div>
+      <h1>Hello from farm</h1>
+      {getAnimals === null ? (
+        <p>Loading...</p>
+      ) : (
+        getAnimals.map((animal) => {
+          return (
+            <div key={animal.id}>
+              <p>{animal.name}</p>
+              <img src={animal.imgUrl} alt="A cute animal" />
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
+};
+
+export default Farm;
---
+++ class0/week2/day4/demo/src/components/Mood.tsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+
+interface MoodProps {
+  startAs: boolean;
+}
+
+const Mood = (props: MoodProps) => {
+  // let mood = true;
+
+  // useEffect takes two arguments:
+  // 1. A function to run when the effect happens
+  // 2. An array of values to keep track of (Dependency array)
+  //    - If it is empty ([]), it will only run when the component gets born
+  // - Every useEffect runs once in the beginning of time
+  const [getMood, setMood] = useState<boolean>(props.startAs);
+
+  // useEffect(() => {
+  //   console.log("Something happened!");
+  // }, []);
+
+  const handleHappy = () => {
+    // mood = true
+    setMood(true);
+    console.log("happy was clicked");
+  };
+
+  const handleSad = () => {
+    setMood(false);
+    console.log("sad was clicked");
+  };
+
+  const handleSwitch = () => {
+    setMood(!getMood);
+    console.log("switch was clicked");
+  };
+
+  return (
+    <div className="mood-box">
+      <h2>{getMood === true ? "🙂" : "😟"}</h2>
+      <button onClick={handleHappy}>Happy</button>
+      <button onClick={handleSad}>Sad</button>
+      <button onClick={handleSwitch}>Switch</button>
+    </div>
+  );
+};
+
+export default Mood;
