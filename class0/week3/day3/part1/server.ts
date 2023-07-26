005d4bf5ed16fed3576dbc592b126cb53d036c93
Aidenbuis <aidenbuis@gmail.com>
2023-07-26 17:54:10 +0200

    Update server

--- class0/week3/day3/part1/server.ts
+++ class0/week3/day3/part1/server.ts
@@ -14,31 +14,44 @@
  * - [X] GET    Read all the animals
  * - [X] GET    Read a specific animal
  *
+ * TO DO Complete our API
+ * - [X] POST   - Create a new animal
+ * - [X] PATCH  - Update an animal
+ * - [X] DELETE - Delete an animal
  */
 
 import express from "express";
+
+/** 👇 We need this to get the JSON body from a request */
+import { json } from "express";
+
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const app = express();
+
+/** 👇 We need this to get the JSON body from a request */
+app.use(json());
+
 const port = 3001;
 
 /**
- * Create some functionality for our server
- * to receive a request to GET all the animals
- *
- * We do that by creating an 'endpoint'
+ * Below here we will define our endpoints.
  */
 
+/**
+ * GET - Get all animals
+ */
 app.get("/animals", async (req, res) => {
   /** We want to get all the data from our db */
   const animals = await prisma.animal.findMany();
   res.send(animals);
 });
 
-/** Endpoint for GET a specific animal */
-/** We need the ID of the animal want to return */
+/**
+ * GET - Get specific animal
+ */
 app.get("/animals/:id", async (req, res) => {
   /** We want to get a specific animal from the db */
   const animalId = parseInt(req.params.id);
@@ -46,7 +59,7 @@ app.get("/animals/:id", async (req, res) => {
   if (isNaN(animalId)) {
     // animalId in URL is not valid!
     res.status(400).send({
-      message: "Wrong request idiot! Send me a number as an id"
+      message: "Wrong request bucko! Send me a number as an id"
     });
   } else {
     // It is valid, hooray!
@@ -66,7 +79,105 @@ app.get("/animals/:id", async (req, res) => {
 });
 
 /**
- * THIS STARTS THE SERVER
+ * POST - Create an animal
+ */
+app.post("/animals", async (req, res) => {
+  const requestBody = req.body;
+
+  /**
+   * Check if the request body is complete
+   */
+  if (
+    "name" in requestBody &&
+    "kind" in requestBody &&
+    "farmId" in requestBody
+  ) {
+    /** Wrap our await call in a try / catch for safety */
+    try {
+      /** Create the animal in the database */
+      const createdAnimal = await prisma.animal.create({
+        data: requestBody
+      });
+
+      /** Respond with the created animal */
+      res.send(createdAnimal);
+    } catch (err) {
+      console.log("Something went wrong.");
+      /** Respond with an error if something went wrong */
+      res.status(500).send("Something went wrong.");
+    }
+  } else {
+    /** Respond with a 400 error if there was a property missing */
+    res.status(400).send("Make sure this has a name, kind and a farmId");
+  }
+});
+
+/**
+ * PATCH - Update an animal
+ */
+app.patch("/animals/:id", async (req, res) => {
+  /** We want to get a specific animal from the db */
+  const animalId = parseInt(req.params.id);
+  const requestBody = req.body;
+
+  if (isNaN(animalId)) {
+    // animalId in URL is not valid!
+    res.status(400).send({
+      message: "Wrong request bucko! Send me a number as an id"
+    });
+  } else {
+    // Run our PATCH for the database
+    try {
+      const updatedAnimal = await prisma.animal.update({
+        where: {
+          id: animalId
+        },
+        data: requestBody
+      });
+
+      res.send(updatedAnimal);
+    } catch (err) {
+      console.log("Something went wrong.");
+      /** Respond with an error if something went wrong */
+      res.status(500).send("Something went wrong.");
+    }
+  }
+});
+
+/**
+ * DELETE - Delete a specific animal
+ */
+app.delete("/animals/:id", async (req, res) => {
+  const animalId = parseInt(req.params.id);
+
+  if (isNaN(animalId)) {
+    // animalId in URL is not valid!
+    res.status(400).send({
+      message: "Wrong request bucko! Send me a number as an id"
+    });
+  } else {
+    try {
+      // Do our database query
+      const deletedAnimal = await prisma.animal.delete({
+        where: {
+          id: animalId
+        }
+      });
+
+      res.send({
+        deletedAnimal: deletedAnimal,
+        message: "Animal Deleted 😭"
+      });
+    } catch (error) {
+      console.log("Something went wrong.");
+      /** Respond with an error if something went wrong */
+      res.status(500).send("Something went wrong.");
+    }
+  }
+});
+
+/**
+ * Start the server! 🚀
  */
 app.listen(port, () => {
   console.log(`🚀 Server started on port ${port}`);
