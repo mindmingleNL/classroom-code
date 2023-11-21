252486c2363b35e6977a14dba58a3970ef75fe73
Aidenbuis <aidenbuis@gmail.com>
2023-11-21 10:50:13 +0100

    day2

--- class1/week3/day2/queriesOne.ts
+++ class1/week3/day2/queriesOne.ts
@@ -1,17 +1,51 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-const addPetWithOwner = async () => {
-  const newPet = await prisma.pet.create({
+const createNewOwner = async () => {
+  // Create a new owner in the prisma database
+  const newOwner = await prisma.owner.create({
     data: {
-      name: "Peluso 🐱",
-      kind: "Cat",
-      owner: {
-        connect: { id: 1 } // Assuming owner with id 1 exists
-      }
+      name: "Aiden",
+      age: 29
     }
   });
-  console.log(newPet);
+
+  console.log("Our new owner:", newOwner);
 };
 
-addPetWithOwner();
+const addPetWithOwner = async () => {
+  try {
+    const newPet = await prisma.pet.create({
+      data: {
+        name: "Peluso 🐱",
+        kind: "Cat",
+        owner: {
+          connect: { id: 1 }
+        }
+      }
+    });
+    console.log(newPet);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const findUniquePet = async (idOfPetToBeFound: number) => {
+  try {
+    // We want to find a unique pet in our prisma database
+    const petFound = await prisma.pet.findUnique({
+      where: {
+        id: idOfPetToBeFound
+      },
+      include: {
+        owner: true // Include the owner relationship
+      }
+    });
+    console.log(petFound);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+findUniquePet(1);
+// addPetWithOwner();
