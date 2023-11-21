50735e8c14cfef8aee0ec47927d9dad77dd49a06
Aidenbuis <aidenbuis@gmail.com>
2023-11-21 13:56:14 +0100

    Afternoon

--- class1/week3/day2/queriesTwo.ts
+++ class1/week3/day2/queriesTwo.ts
@@ -3,17 +3,24 @@ const prisma = new PrismaClient();
 
 async function addSongLike() {
   // Assuming the song with id 1 and user with id 1 exist
-  await prisma.song.update({
-    where: {
-      id: 1
-    },
-    data: {
-      likedBy: {
-        connect: { id: 1 }
+  try {
+    const updatedSong = await prisma.song.update({
+      where: {
+        id: 1
+      },
+      data: {
+        likedByUser: {
+          connect: { id: 1 }
+        }
       }
-    }
-  });
+    });
+    console.log(updatedSong);
+  } catch (err) {
+    console.log(err);
+  }
 }
+
+// addSongLike();
 
 async function queryLikes() {
   const userWithLikes = await prisma.user.findUnique({
@@ -24,7 +31,21 @@ async function queryLikes() {
       likes: true
     }
   });
+  console.log(userWithLikes);
 }
 
-addSongLike();
-queryLikes();
+async function querySong() {
+  const song = await prisma.song.findUnique({
+    where: {
+      id: 1
+    },
+    include: {
+      likedByUser: true
+    }
+  });
+  console.log(song);
+}
+
+// addSongLike();
+// queryLikes();
+querySong();
