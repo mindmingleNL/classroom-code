c2a090ee2feeee79350204fb3ae8346d5295c775
Aidenbuis <aidenbuis@gmail.com>
2023-11-26 16:36:17 +0100

    prep

--- pzh0/day2/script.js
+++ pzh0/day2/script.js
@@ -1,38 +1,46 @@
 //////////////
-// Example 1
-// Function to double the value
-function double(x) {
-    return x * 2;
-}
-// console.log(double("4"));
-//////////////
-// Example 2
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-// console.log(multiply("6", "3"));
-//////////////
-// Example 3
-var checkVergunningVoorTanker = function (tanker) {
-    var volume = tanker.volume, liters = tanker.liters, materiaal = tanker.materiaal, diesel = tanker.diesel, gewicht = tanker.gewicht, bemanning = tanker.bemanning;
-    if (liters < 50) {
-        return true;
-    }
-    else {
-        return false;
-    }
+// Working with objects
+
+const checkVergunningVoorTanker = (tanker) => {
+  const { volume, liters, materiaal, diesel, gewicht, bemanning } = tanker;
+  if (liters < 50) {
+    return true;
+  } else {
+    return false;
+  }
 };
-var resultVergunning = checkVergunningVoorTanker({
-    volume: 100,
-    liters: "12",
-    materiaal: 65,
-    diesel: true,
-    gewicht: 12,
-    bemanning: 80
+
+const resultVergunning = checkVergunningVoorTanker({
+  volume: 100,
+  liters: "12",
+  materiaal: 65,
+  diesel: true,
+  gewicht: 12,
+  bemanning: 80
 });
-// console.log(resultVergunning);
+
+console.log(resultVergunning);
+
 //////////////
-// Example 4 - Inference
-var obj = { width: 10, height: 15 };
-var area = obj.width * obj.heigth;
-// console.log(area);
+// Inference
+
+const obj = { width: 10, height: 15 };
+const area = obj.width * obj.heigth;
+
+//////////////
+// Optional Properties
+
+const user = {
+  name: "John",
+  id: 0
+};
+
+//////////////
+// Basics
+// Number types
+// String types
+// Booleans types
+// 'special' types
+
+//////////////
+// Typing an Array
