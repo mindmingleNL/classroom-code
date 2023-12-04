a27c79c4b6729a553f1de64343b3cf83830ee42a
Aidenbuis <aidenbuis@gmail.com>
2023-12-04 11:39:46 +0100

    CSS Day

---
+++ class1/week5/day01/src/pages/index.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <>
+      <main>
+        {/* <span className="square">Hello World!</span> */}
+        <nav>
+          <ul className="nav-list">
+            <li className="message-incoming">Item1</li>
+            <li className="message-outcoming">Item2</li>
+            <li className="message-incoming">Item3</li>
+            <li className="message-outcoming">Item4</li>
+            <li className="message-incoming">Item5</li>
+          </ul>
+        </nav>
+        <header>
+          <h1>Hello World</h1>
+        </header>
+      </main>
+    </>
+  );
+}
