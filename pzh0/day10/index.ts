46d4b3c51581f74c7d89a528b5fbfd4111e13e3c
Swen Mulderij <swenmulderij@gmail.com>
2024-01-23 09:06:46 +0100

    after lecture

--- pzh0/day10/index.ts
+++ pzh0/day10/index.ts
@@ -9,11 +9,74 @@ const defaultPetSelect: Prisma.PetSelect = {
 };
 
 const query = async () => {
-  await prisma.pet.findMany({ select: { ...defaultPetSelect, kind: true } });
-  const kindsWithPets = await prisma.kind.findMany({
-    select: { name: true },
+  const newPet = await prisma.pet.update({
+    where: {
+      id: 1,
+    },
+    data: {
+      Pet_Food: {
+        connect: {
+          id: 1,
+        },
+      },
+    },
   });
-  console.log(JSON.stringify(kindsWithPets));
+
+  const petFoodRelation = await prisma.pet_Food.findFirst({
+    where: {
+      petId: newPet.id,
+    },
+  });
+
+  if (!petFoodRelation) {
+    return;
+  }
+
+  await prisma.pet_Food.update({
+    where: {
+      id: petFoodRelation.id,
+    },
+    data: {
+      rating: 5,
+    },
+  });
+
+  // const petFound = await prisma.pet.findFirst({
+  //   where: {
+  //     name: "Fifi",
+  //   },
+  //   select: {
+  //     ...defaultPetSelect,
+  //     kind: { select: { name: true } },
+  //     owners: { select: { name: true } },
+  //   },
+  // });
+  // console.log(petFound);
+  // const newPet = await prisma.pet.create({
+  //   data: {
+  //     age: 1,
+  //     name: "Fifi",
+  //     tame: false,
+  //     kindId: 1,
+  //     owners: {
+  //       create: {
+  //         name: "Alice",
+  //       },
+  //       connect: [
+  //         {
+  //           id: 1,
+  //         },
+  //         { id: 2 },
+  //         { id: 3 },
+  //       ],
+  //     },
+  //   },
+  // });
+  // await prisma.pet.findMany({ select: { ...defaultPetSelect, kind: true } });
+  // const kindsWithPets = await prisma.kind.findMany({
+  //   select: { name: true },
+  // });
+  // console.log(JSON.stringify(kindsWithPets));
   // await prisma.pet.create({
   //   data: {
   //     name: "Samson",
--- pzh0/day10/prisma/seed.ts
+++ pzh0/day10/prisma/seed.ts
@@ -1,5 +1,8 @@
 import petData from "./data/pets.json";
 import kindData from "./data/kinds.json";
+import ownerData from "./data/owners.json";
+import foodData from "./data/foods.json";
+
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -13,6 +16,16 @@ const seed = async () => {
   console.log("Seeding pets");
   for (let i = 0; i < petData.length; i++) {
     await prisma.pet.create({ data: petData[i] });
+  }
+
+  console.log("Seeding owners");
+  for (let i = 0; i < ownerData.length; i++) {
+    await prisma.owner.create({ data: ownerData[i] });
+  }
+
+  console.log("Seeding foods");
+  for (let i = 0; i < foodData.length; i++) {
+    await prisma.food.create({ data: foodData[i] });
   }
 
   console.log("Seeding done");
