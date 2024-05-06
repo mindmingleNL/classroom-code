6fbd61ba9068bc3dd997c2b63ffbd585da003cc3
Swen Mulderij <swenmulderij@gmail.com>
2024-05-06 13:02:46 +0200

    after day1lecture1

---
+++ class2/week1/day1/_main.py
@@ -0,0 +1,4 @@
+example = 10
+something = 0
+
+print(example / something)
---
+++ class2/week1/day1/main.js
@@ -0,0 +1,17 @@
+// const student = {
+//   name: "Nargiz",
+//   cat: {
+//     name: "Logan",
+//     gender: false,
+//   },
+//   classNo: 2,
+// };
+
+// const class2 = ["Mark", "Sneha", "Brandon", "Nargiz", "Trang"];
+
+// console.log(class2[class2.length - 1]);
+
+const a = ["Pig", "Cow"];
+const b = { name: "bla" };
+
+console.log(a + b);
