b4d3b39a7e74e42445e5ab3fd3c1d7f272988dfc
Swen Mulderij <swenmulderij@gmail.com>
2024-05-23 11:14:21 +0200

    after seeding

---
+++ class2/week3/day3/prisma/seed.ts
@@ -0,0 +1,29 @@
+import plantsData from "./seeddata/plants.json";
+import usersData from "./seeddata/users.json";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const seed = async () => {
+  // Seed every user
+  console.log("Seeding Users...");
+  for (let i = 0; i < usersData.length; i++) {
+    const thisUser = usersData[i];
+    await prisma.user.create({
+      data: thisUser,
+    });
+  }
+
+  // Seed every plant
+  console.log("Seeding Plants...");
+  for (let i = 0; i < plantsData.length; i++) {
+    const thisPlant = plantsData[i];
+    await prisma.plants.create({
+      data: thisPlant,
+    });
+  }
+
+  console.log("Done!");
+};
+
+seed();
