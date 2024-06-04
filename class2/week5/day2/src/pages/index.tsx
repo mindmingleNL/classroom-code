75ffd27ea564d8cecf34474c6174b1da6813f5af
Yoeran Luteijn <contact@yoeran.nl>
2024-06-04 14:20:24 +0200

    add css and html for challenge 2

--- class2/week5/day2/src/pages/index.tsx
+++ class2/week5/day2/src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { NewsCard } from "@/components/NewsCard";
+
 export default function Home() {
   return (
     <>
@@ -31,25 +33,14 @@ export default function Home() {
         </ul>
       </nav>
 
-      <p className="long-content">
-        Repellendus quaerat repellat quibusdam molestiae et dolorem sint animi.
-        Et libero quo asperiores delectus pariatur molestiae at ipsa. Sed
-        similique accusantium ratione architecto alias ducimus doloremque.
-        Veritatis debitis nam ut itaque. Saepe voluptatibus ad earum vel iusto
-        perspiciatis. Et unde sit sit et et delectus soluta. Ea et velit vitae
-        eum pariatur quae. Amet nobis modi sunt distinctio at similique. Fugiat
-        dolore vel esse velit possimus ut harum. Occaecati unde quia aliquid
-        repudiandae distinctio. Et nihil doloremque sint a cum voluptatem. Omnis
-        sunt qui deleniti omnis cupiditate molestias optio. Repellat ducimus
-        quasi animi repudiandae quis cumque quia cupiditate. Et perspiciatis
-        deserunt dignissimos ipsum quod aut. Quia corporis possimus est.
-        Eligendi reprehenderit debitis maxime. Facere ut tenetur tempore
-        architecto similique perspiciatis qui. Quae enim suscipit aspernatur.
-        Incidunt qui dolorum at tempore. Qui repudiandae dolorem modi incidunt
-        id quis dignissimos. Sequi ab nobis debitis. Quo dolores ea et non.
-        Accusamus quidem non libero nemo ipsam qui. Rem autem voluptates
-        consectetur.
-      </p>
+      <div className="news-grid">
+        <NewsCard title="Fake news: Trang is married" />
+        <NewsCard title="The weather is shit" />
+        <NewsCard title="Brandon has a time limit" />
+        <NewsCard title="Nargiz is secretly a Russian spy" />
+        <NewsCard title="Mark just won the Grand Prix" />
+        <NewsCard title="Yoeran was late for class" />
+      </div>
     </>
   );
 }
